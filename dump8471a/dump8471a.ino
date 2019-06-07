3f96788c2a6ac229a7e90ebbe6c300255a362560
abzman <abzman2000@gmail.com>
2019-06-07 07:56:55 -0400

    revision1

--- dump8471a/dump8471a.ino
+++ dump8471a/dump8471a.ino
@@ -13,7 +13,7 @@ uint8_t payload[1024] =
   0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF, 0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF, 0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF
 
 } ;
-
+int d = 5;
 int VDD = A0;
 int PROG = A1;
 int TEST0 = A2;
@@ -57,10 +57,6 @@ void setup() {
   digitalWrite(TEST0, HIGH); // Set TEST0 to +5V
   digitalWrite(EA, LOW); // Set EA to +5V
   digitalWrite(RESET, LOW); // Set RESET to 0V
-  digitalWrite(LED, HIGH);
-  delay(3000);
-  digitalWrite(LED, LOW);
-  delay(1000);
   Serial.println("MCS-48 Programmer READY");
 }
 
@@ -68,17 +64,19 @@ void loop() {
   if (digitalRead(BUTTON) == LOW) {
     Serial.println("button pressed");
     delay(1000);
-    /*
+    
     Serial.println("Dumping Chip in 5 seconds");
     delay(5000);
     dump();
+    /*
     Serial.println("Verifying Chip in 5 seconds");
     delay(5000);
     verify();
-    */
+    
     Serial.println("Programming Chip in 5 Seconds");
     delay(5000);
     programverify();
+    */
   }
   /*
     // put your main code here, to run repeatedly:
@@ -145,9 +143,9 @@ void programverify() {
     digitalWrite(P21, i & 0x0200);
     digitalWrite(P22, i & 0x0400);
 
-    delay(10);                         // Wait 10ms to have stable lines (Taw)
+    delayMicroseconds(d);                         // Wait 10ms to have stable lines (Taw)
     digitalWrite(RESET, HIGH);        // Latch the Address into the MCS-48 µC
-    delay(10);                         // Give the µC some time to latch the Address (Twa)
+    delayMicroseconds(d);                         // Give the µC some time to latch the Address (Twa)
 
     Serial.print("   For Data ");
     Serial.print(payload[i], HEX);
@@ -161,15 +159,15 @@ void programverify() {
     digitalWrite(DataBus6, payload[i] & 0x0040);
     digitalWrite(DataBus7, payload[i] & 0x0080);
 
-    delay(10);                         // Wait 10ms to have stable lines (Tdw)
+    delayMicroseconds(d);                      // Wait 10ms to have stable lines (Tdw)
     digitalWrite(VDD, HIGH); //getting ready to program
-    delay(10);                         // Wait 10ms to have stable lines (Tvddw)
+    delayMicroseconds(d);                          // Wait 10ms to have stable lines (Tvddw)
     digitalWrite(PROG, HIGH); //programming
-    delay(10);                         // Wait 10ms to have stable lines (Tpw)
+    delayMicroseconds(d);                          // Wait 10ms to have stable lines (Tpw)
     digitalWrite(PROG, LOW); //programming over
-    delay(10);                         // Wait 10ms to have stable lines (Tvddh)
+    delayMicroseconds(d);                         // Wait 10ms to have stable lines (Tvddh)
     digitalWrite(VDD, LOW); //finish programming
-    delay(10);                         // Wait 10ms to have stable lines (Twt)
+    delayMicroseconds(d);                         // Wait 10ms to have stable lines (Twt)
 
     //verifying
 
@@ -182,7 +180,7 @@ void programverify() {
     pinMode(DataBus6, INPUT_PULLUP);
     pinMode(DataBus7, INPUT_PULLUP);
     digitalWrite(TEST0, HIGH); //test0 to 5v for reading
-    delay(25);                         // Wait 25ms to have stable lines (Tdo)
+    delayMicroseconds(d);                         // Wait 25ms to have stable lines (Tdo)
     uint8_t VERIFY = 0;
     if (digitalRead(DataBus0)) {
       VERIFY = VERIFY + 0x01;
@@ -226,7 +224,7 @@ void programverify() {
   digitalWrite(TEST0, LOW);
   digitalWrite(EA, LOW);
 }
-
+/*
 void verify() {
 
   pinMode(LED, OUTPUT);
@@ -272,9 +270,9 @@ void verify() {
     digitalWrite(P21, i & 0x0200);
     digitalWrite(P22, i & 0x0400);
 
-    delay(10);                         // Wait 1ms to have stable lines (Taw)
+    delayMicroseconds(d);                         // Wait 1ms to have stable lines (Taw)
     digitalWrite(RESET, HIGH);        // Latch the Address into the MCS-48 µC
-    delay(10);                         // Give the µC some time to latch the Address (Twa)
+    delayMicroseconds(d);                         // Give the µC some time to latch the Address (Twa)
 
     Serial.print("   For Data ");
     Serial.print(payload[i], HEX);
@@ -287,7 +285,7 @@ void verify() {
     pinMode(DataBus5, INPUT_PULLUP);
     pinMode(DataBus6, INPUT_PULLUP);
     pinMode(DataBus7, INPUT_PULLUP);
-    delay(25);
+    delayMicroseconds(d);
     uint8_t VERIFY = 0;
     if (digitalRead(DataBus0)) {
       VERIFY = VERIFY + 0x01;
@@ -331,7 +329,7 @@ void verify() {
   digitalWrite(TEST0, LOW);
   digitalWrite(EA, LOW);
 }
-
+*/
 
 void dump() {
 
@@ -359,38 +357,104 @@ void dump() {
     pinMode(DataBus6, OUTPUT);
     pinMode(DataBus7, OUTPUT);
     //Serial.print("Reading Address ");
-    Serial.print(i, HEX);
+    //Serial.print(i, HEX);
 
 
     digitalWrite(RESET, LOW); //reset low while setting addresses
 
 
     //set address lines to bits in the address (remember, we have 10 bits in the 8041 and 11 in the 8042)
-    digitalWrite(DataBus0, i & 0x0001);
-    digitalWrite(DataBus1, i & 0x0002);
-    digitalWrite(DataBus2, i & 0x0004);
-    digitalWrite(DataBus3, i & 0x0008);
-    digitalWrite(DataBus4, i & 0x0010);
-    digitalWrite(DataBus5, i & 0x0020);
-    digitalWrite(DataBus6, i & 0x0040);
-    digitalWrite(DataBus7, i & 0x0080);
-    digitalWrite(P20, i & 0x0100);
-    digitalWrite(P21, i & 0x0200);
-    digitalWrite(P22, i & 0x0400);
+    if((i & 0x0001) >0)
+    {
+    digitalWrite(DataBus0, 1);
+    } else {
+      
+    digitalWrite(DataBus0, 0);
+    }
+        if((i & 0x0002) >0)
+    {
+    digitalWrite(DataBus1, 1);
+    } else {
+      
+    digitalWrite(DataBus1, 0);
+    }
+        if((i & 0x0004) >0)
+    {
+    digitalWrite(DataBus2, 1);
+    } else {
+      
+    digitalWrite(DataBus2, 0);
+    }
+        if((i & 0x0008) >0)
+    {
+    digitalWrite(DataBus3, 1);
+    } else {
+      
+    digitalWrite(DataBus3, 0);
+    }
+        if((i & 0x0010) >0)
+    {
+    digitalWrite(DataBus4, 1);
+    } else {
+      
+    digitalWrite(DataBus4, 0);
+    }
+        if((i & 0x0020) >0)
+    {
+    digitalWrite(DataBus5, 1);
+    } else {
+      
+    digitalWrite(DataBus5, 0);
+    }
+        if((i & 0x0040) >0)
+    {
+    digitalWrite(DataBus6, 1);
+    } else {
+      
+    digitalWrite(DataBus6, 0);
+    }
+        if((i & 0x0080) >0)
+    {
+    digitalWrite(DataBus7, 1);
+    } else {
+      
+    digitalWrite(DataBus7, 0);
+    }
+        if((i & 0x0100) >0)
+    {
+    digitalWrite(P20, 1);
+    } else {
+      
+    digitalWrite(P20, 0);
+    }
+        if((i & 0x0200) >0)
+    {
+    digitalWrite(P21, 1);
+    } else {
+      
+    digitalWrite(P21, 0);
+    }
+        if((i & 0x0400) >0)
+    {
+    digitalWrite(P22, 1);
+    } else {
+      
+    digitalWrite(P22, 0);
+    }
 
-    delay(10);                         // Wait 1ms to have stable lines (Taw)
+    delayMicroseconds(d);                         // Wait 1ms to have stable lines (Taw)
     digitalWrite(RESET, HIGH);        // Latch the Address into the MCS-48 µC
-    delay(10);                         // Give the µC some time to latch the Address (Twa)
+    delayMicroseconds(d);                         // Give the µC some time to latch the Address (Twa)
 
-    pinMode(DataBus0, INPUT_PULLUP);
-    pinMode(DataBus1, INPUT_PULLUP);
-    pinMode(DataBus2, INPUT_PULLUP);
-    pinMode(DataBus3, INPUT_PULLUP);
-    pinMode(DataBus4, INPUT_PULLUP);
-    pinMode(DataBus5, INPUT_PULLUP);
-    pinMode(DataBus6, INPUT_PULLUP);
-    pinMode(DataBus7, INPUT_PULLUP);
-    delay(25);
+    pinMode(DataBus0, INPUT);
+    pinMode(DataBus1, INPUT);
+    pinMode(DataBus2, INPUT);
+    pinMode(DataBus3, INPUT);
+    pinMode(DataBus4, INPUT);
+    pinMode(DataBus5, INPUT);
+    pinMode(DataBus6, INPUT);
+    pinMode(DataBus7, INPUT);
+    delayMicroseconds(d);
     uint8_t VERIFY = 0;
     if (digitalRead(DataBus0)) {
       VERIFY = VERIFY + 0x01;
@@ -417,15 +481,22 @@ void dump() {
       VERIFY = VERIFY + 0x80;
     }
 
-
-    Serial.print(" : ");
-    Serial.println(VERIFY, HEX);
+    delayMicroseconds(d);
+    //Serial.print(" : ");
+    //Serial.println(VERIFY, HEX);
+    Serial.print("0x");
+    Serial.print(VERIFY, HEX);
+    Serial.print(", ");
     digitalWrite(RESET, LOW);
-
+    delayMicroseconds(d);
   }
   digitalWrite(TEST0, LOW);
   digitalWrite(EA, LOW);
+  
+    Serial.println();
+    Serial.println("done");
 }
+/*
 void test() {
   Serial.println("pulsing VDD");
   delay(3500);
@@ -489,3 +560,4 @@ void done() {
     digitalWrite(LED, LOW);
   }
 }
+*/
